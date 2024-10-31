170002618e462c858c6327c6f41471a877c38ef1
searchcolor <qkrdydwo1022@gmail.com>
2024-10-31 19:47:42 +0900

    feat: add query
    schedule.sql
    query 추가

--- schedule.sql
+++ schedule.sql
@@ -4,7 +4,7 @@ CREATE DATABASE schedule DEFAULT CHARSET = utf8 COLLATE =utf8_bin;
 USE schedule;
 DROP TABLE IF EXISTS user;
 CREATE TABLE `user` (
-                        `ID` int NOT NULL AUTO_INCREMENT,
+                        `id` int NOT NULL AUTO_INCREMENT,
                         `email` varchar(255) DEFAULT NULL,
                         `name` varchar(45) DEFAULT NULL,
                         `registration_date` date DEFAULT NULL,
@@ -14,13 +14,31 @@ CREATE TABLE `user` (
 
 DROP TABLE IF EXISTS schedule;
 CREATE TABLE `schedule` (
-                            `ID` int NOT NULL AUTO_INCREMENT,
+                            `id` int NOT NULL AUTO_INCREMENT,
                             `password` varchar(255) DEFAULT NULL,
-                            `user_ID` int DEFAULT NULL,
+                            `user_id` int DEFAULT NULL,
                             `user_name` varchar(45) DEFAULT NULL,
+                            `detail` varchar(255) DEFAULT NULL,
                             `registration_date` date DEFAULT NULL,
                             `modification_date` date DEFAULT NULL,
-                            PRIMARY KEY (`ID`),
-                            KEY `userID_idx` (`user_ID`),
-                            CONSTRAINT `userID` FOREIGN KEY (`user_ID`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+                            PRIMARY KEY (`id`),
+                            KEY `userID_idx` (`user_id`),
+                            CONSTRAINT `userID` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+#일정 생성 쿼리 예시
+Insert into schedule (password, user_id, user_name, registration_date, modification_date)
+values ('test1234','test_id','test_name','2024-10-31','2024-10-31');
+
+#전체일정 조회
+Select * from schedule;
+
+#선택일정 조회
+Select * from schedule where id = 'select_schedule_id';
+
+#선택일정 수정
+Update schedule set detail='modify_detail' where id ='select_schedule_id';
+
+#선택일정 삭제
+Delete from schedule where id = 'select_schedule_id';
